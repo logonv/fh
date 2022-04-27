d4388d66f4665d7923a9fc803252c259b2baceea
logonv <logonv@users.noreply.github.com>
2022-04-27 10:19:54 +0100

    make album card and display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LandingPage from './views/LandingPage';
+// import AlbumCard from './components/AlbumCard/AlbumCard';
 
 function App() {
   return (
-    <div>Blank</div>
-
+    <LandingPage />
   );
 }
 
---
+++ src/views/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AlbumCard from '../components/AlbumCard/AlbumCard'
+
+const LandingPage = () => {
+  return (
+    <div id = 'albums'><AlbumCard/>
+    <AlbumCard/>
+    </div>
+  )
+}
+
+export default LandingPage
